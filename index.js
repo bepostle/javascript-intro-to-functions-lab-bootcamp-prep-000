3227e3fc761e6f325d2e444a3be496fcc731ad1d
Brent Postlethweight <brentpostle@gmail.com>
2018-09-03 20:24:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(whisper("HELLO"));
 }
+
+function sayHiToGrandma(string) {
+  if (string === lowerCase) {
+  return "I can\'t hear you!";
+  } else if (string === upperCase) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
